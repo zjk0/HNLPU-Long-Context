29f6c3ef5035ddddea0bc9721a5c88157c54647f
zjk0 <2054295479@qq.com>
2026-07-04 21:50:15 +0800

    let some parameters can be set in cmd

--- performance_eval_simplify_my.py
+++ performance_eval_simplify_my.py
@@ -1,12 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
+import argparse
 
-'''
-@brief: parse the parameters from yaml file.
-@param: param_yaml_path: the path to the yaml file.
-@return: a dictionary of parameters in the yaml file.
-'''
+def parse_args(hnlpu_parameters):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--kv_cache_dtype_bytes", type = int, default = hnlpu_parameters["memory"]["kv_dtype_bytes"])
+    parser.add_argument("--plot_path", type = str, default = "./performance_vs_context.png")
+    parser.add_argument("--hbm_bandwidth", type = int, default = hnlpu_parameters["memory"]["hbm_bandwidth_GBps_per_chip"], help = "unit: GB/s")
+    parser.add_argument("--batch_size", type = int, default = hnlpu_parameters["hnlpu"]["max_batch_size"])
+
+    args = parser.parse_args()
+    return args
+
 def parse_parameters(param_yaml_path):
     with open(param_yaml_path, "r", encoding="utf-8") as f:
         parameters = yaml.safe_load(f)
@@ -23,14 +30,14 @@ def additional_memory_stall_eval(
         return 0.0
     else:
         ratio = exposed_hbm_stall_fraction_at_512k * ((context_length - stall_hidden_until_context) / (524288 - stall_hidden_until_context))
+        ratio = min(max(ratio, 0.0), 0.95)
         return non_stall_time * (ratio / (1 - ratio))
 
 def throughput_eval(
     context_length, 
     parameters,
-    batch_size = None
+    batch_size
 ):
-    batch_size = parameters["hnlpu"]["max_batch_size"] if batch_size is None else batch_size
     calibration_context_length = parameters["hnlpu"]["calibration_context_length"]
     reported_throughput = parameters["hnlpu"]["reported_throughput_tokens_per_s_at_2k"]
     reported_time = batch_size / reported_throughput
@@ -62,15 +69,15 @@ def kv_cache_size_eval(
 
 def memory_access_latency_eval(
     parameters, 
+    attention_buffer_bandwidth, 
+    attention_buffer_size, 
+    hbm_bandwidth, 
     kv_cache, 
 ):
-    attention_buffer_bandwidth = parameters["memory"]["attention_buffer_bandwidth_TBps_per_chip"]
-    attention_buffer_size = parameters["memory"]["attention_buffer_mb_per_chip"]
     kv_cache_per_chip = kv_cache / parameters["hnlpu"]["num_chips"]
     if kv_cache_per_chip <= attention_buffer_size * (1024 ** 2):
         return kv_cache_per_chip / (attention_buffer_bandwidth * 1e12) * 1e3
     else:
-        hbm_bandwidth = parameters["memory"]["hbm_bandwidth_GBps_per_chip"]
         hbm_kv_cache_per_chip = kv_cache_per_chip - attention_buffer_size * (1024 ** 2)
         attention_buffer_latency = (attention_buffer_size * (1024 ** 2)) / (attention_buffer_bandwidth * 1e12) * 1e3
         hbm_latency = hbm_kv_cache_per_chip / (hbm_bandwidth * 1e9) * 1e3
@@ -111,6 +118,7 @@ def plot_performance_vs_context_length(
 
 def main():
     parameters = parse_parameters("./hnlpu_config.yaml")
+    cmd_args = parse_args(parameters)
     context_start = parameters["eval"]["context_lengths"]["start"]
     context_stop = parameters["eval"]["context_lengths"]["stop"]
     context_num = parameters["eval"]["context_lengths"]["num"]
@@ -128,8 +136,8 @@ def main():
             parameters["model"]["num_kv_heads"], 
             parameters["model"]["head_dim"], 
             parameters["model"]["num_layers"], 
-            parameters["memory"]["kv_dtype_bytes"], 
-            parameters["hnlpu"]["max_batch_size"]
+            cmd_args.kv_cache_dtype_bytes, 
+            cmd_args.batch_size
         )
         performance["kv cache size"].append(kv_cache / (1024 ** 3))
         
@@ -138,23 +146,27 @@ def main():
             parameters["model"]["num_q_heads"], 
             parameters["model"]["head_dim"], 
             parameters["model"]["num_layers"], 
-            parameters["hnlpu"]["max_batch_size"]
+            cmd_args.batch_size
         )
         performance["attention compute cost"].append(attention_compute_cost)
         
         memory_access_latency_ms = memory_access_latency_eval(
             parameters, 
+            parameters["memory"]["attention_buffer_bandwidth_TBps_per_chip"], 
+            parameters["memory"]["attention_buffer_mb_per_chip"], 
+            cmd_args.hbm_bandwidth, 
             kv_cache
         )
         performance["memory access latency"].append(memory_access_latency_ms)
         
         throughput = throughput_eval(
             context_length, 
-            parameters
+            parameters, 
+            cmd_args.batch_size
         )
         performance["throughput"].append(throughput)
         
-    plot_performance_vs_context_length(performance, context_lengths, "./performance_vs_context.png")
+    plot_performance_vs_context_length(performance, context_lengths, cmd_args.plot_path)
         
 if __name__ == "__main__":
     main()
