4b70521fe95b0d2b786d5884f4911cfa4f1ee49c
zjk0 <2054295479@qq.com>
2026-07-07 11:28:12 +0800

    plan to start building hnlpu simulator

--- exp_bash/batch_size_exp.sh
+++ exp_bash/batch_size_exp.sh
@@ -1,5 +1,5 @@
-python ./performance_eval_simplify_my.py --batch_size 16 --plot_path ./plots/performance_batch_size_16.png
-python ./performance_eval_simplify_my.py --batch_size 32 --plot_path ./plots/performance_batch_size_32.png
-python ./performance_eval_simplify_my.py --batch_size 64 --plot_path ./plots/performance_batch_size_64.png
-python ./performance_eval_simplify_my.py --batch_size 128 --plot_path ./plots/performance_batch_size_128.png
-python ./performance_eval_simplify_my.py --batch_size 216 --plot_path ./plots/performance_batch_size_216.png
+python ./simplified_eval/performance_eval_simplify_my.py --batch_size 16 --plot_path ./plots/performance_batch_size_16.png
+python ./simplified_eval/performance_eval_simplify_my.py --batch_size 32 --plot_path ./plots/performance_batch_size_32.png
+python ./simplified_eval/performance_eval_simplify_my.py --batch_size 64 --plot_path ./plots/performance_batch_size_64.png
+python ./simplified_eval/performance_eval_simplify_my.py --batch_size 128 --plot_path ./plots/performance_batch_size_128.png
+python ./simplified_eval/performance_eval_simplify_my.py --batch_size 216 --plot_path ./plots/performance_batch_size_216.png
