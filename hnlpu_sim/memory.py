83418fa8ab4c54095c806baaa7d4df62c909e1b6
zjk0 <2054295479@qq.com>
2026-07-13 23:32:00 +0800

    coding memory.py

--- hnlpu_sim/memory.py
+++ hnlpu_sim/memory.py
@@ -1,45 +1,56 @@
 class Memory:
-    def __init__(self, size_mb, bandwidth_gb_per_s):
-        self.size_mb = size_mb
-        self.bandwidth_gb_per_s = bandwidth_gb_per_s
-        self.usage_mb = 0
+    def __init__(self, memory_type, size_byte, bandwidth_byte_per_s, fixed_latency_s):
+        self.memory_type = memory_type
+        self.size_byte = size_byte
+        self.bandwidth_byte_per_s = bandwidth_byte_per_s
+        self.fixed_latency_s = fixed_latency_s
+        self.usage_byte = 0
+        
+    def get_total_size(self):
+        return self.size_byte
+    
+    def get_usage(self):
+        return self.usage_byte
+    
+    def get_remain_size(self):
+        return self.size_byte - self.usage_byte
         
     def is_full(self):
-        if self.usage_mb >= self.size_mb:
+        if self.usage_byte >= self.size_byte:
             return True
         else:
             return False
         
     def is_empty(self):
-        if self.usage_mb == 0:
+        if self.usage_byte == 0:
             return True
         else:
             return False
         
-    def can_allocate(self, allocate_size_mb):
-        if self.usage_mb + allocate_size_mb > self.size_mb:
+    def can_allocate(self, allocate_size_byte):
+        if self.usage_byte + allocate_size_byte > self.size_byte:
             return False
         else:
             return True
         
-    def can_free(self, free_size_mb):
-        if self.usage_mb - free_size_mb < 0:
+    def can_free(self, free_size_byte):
+        if self.usage_byte - free_size_byte < 0:
             return False
         else:
             return True
         
-    def allocate_memory(self, allocate_size_mb):
-        if self.can_allocate(allocate_size_mb):
-            self.usage_mb += allocate_size_mb
+    def allocate_memory(self, allocate_size_byte):
+        if self.can_allocate(allocate_size_byte):
+            self.usage_byte += allocate_size_byte
             return True
         else:
-            self.usage_mb = self.size_mb
+            self.usage_byte = self.size_byte
             return False
         
-    def free_memory(self, free_size_mb):
-        if self.can_free(free_size_mb):
-            self.usage_mb -= free_size_mb
+    def free_memory(self, free_size_byte):
+        if self.can_free(free_size_byte):
+            self.usage_byte -= free_size_byte
             return True
         else:
-            self.usage_mb = 0
+            self.usage_byte = 0
             return False
