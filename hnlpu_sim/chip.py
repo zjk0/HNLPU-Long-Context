d392af8638c925a4ac362ae09b78015865f53d50
zjk0 <2054295479@qq.com>
2026-07-13 12:38:37 +0800

    adjust the structure of hnlpu_sim

--- hnlpu_sim/chip.py
+++ hnlpu_sim/chip.py
@@ -1,48 +1,5 @@
-class Memory:
-    def __init__(self, size_mb, bandwidth_gb_per_s):
-        self.size_mb = size_mb
-        self.bandwidth_gb_per_s = bandwidth_gb_per_s
-        self.usage_mb = 0
-        
-    def is_full(self):
-        if self.usage_mb >= self.size_mb:
-            return True
-        else:
-            return False
-        
-    def is_empty(self):
-        if self.usage_mb == 0:
-            return True
-        else:
-            return False
-        
-    def can_allocate(self, allocate_size_mb):
-        if self.usage_mb + allocate_size_mb > self.size_mb:
-            return False
-        else:
-            return True
-        
-    def can_free(self, free_size_mb):
-        if self.usage_mb - free_size_mb < 0:
-            return False
-        else:
-            return True
-        
-    def allocate_memory(self, allocate_size_mb):
-        if self.can_allocate(allocate_size_mb):
-            self.usage_mb += allocate_size_mb
-            return True
-        else:
-            self.usage_mb = self.size_mb
-            return False
-        
-    def free_memory(self, free_size_mb):
-        if self.can_free(free_size_mb):
-            self.usage_mb -= free_size_mb
-            return True
-        else:
-            self.usage_mb = 0
-            return False
+from memory import Memory
+from kv_cache import KVcacheBlock, KVcacheManager
 
 class Chip:
     def __init__(
---
+++ hnlpu_sim/kv_cache.py
@@ -0,0 +1,7 @@
+class KVcacheBlock:
+    def __init__(self):
+        pass
+    
+class KVcacheManager:
+    def __init__(self):
+        pass
